dfa04a611c2b75baae81d0ede62f4fac067c9ead
Nic McDonald <nicmcd@users.noreply.github.com>
2019-03-27 14:47:10 -0700

    Update Exception.cc
    Code cleanup

--- src/ex/Exception.cc
+++ src/ex/Exception.cc
@@ -50,7 +50,7 @@ std::string vstrprintf(const char* _format, va_list _args) {
   char* buffer = new char[size];
 
   // printf into the buffer
-  int size2 = vsnprintf(buffer, (size_t)size,  _format, args2);
+  int size2 = vsnprintf(buffer, (size_t)size, _format, args2);
   (void)size2;
   assert((size2 + 1) >= size);
 
